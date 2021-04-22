e92d892d175848e7d9c162e0e788ef8211c66261
bailey watkins <bailey.watkins@rocketmail.com>
2021-04-21 21:14:09 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+const employee={
+    name:['Bailey'],
+    streetAddress:['123 Sesame Street']
+}
+ 
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee
+    
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee
+
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newKey = {...employee};
+    delete newKey[key];
+    console.log (newKey);
+    return newKey
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name;
+    console.log(employee);
+    return employee
+}
